0107a59835b81b05799e6f84f520b535f2a38344
Clayton Lewis <lewisclayton89@gmail.com>
2020-08-16 17:08:20 -0400

    progress

--- js-files/slideshow.js
+++ js-files/slideshow.js
@@ -1,4 +1,5 @@
 const realPic = document.getElementById("realPic1");
+let slidesMoving = true
 
 let i = 0;
 let picArray = [
@@ -11,28 +12,37 @@ let picArray = [
 
 realPic.src = picArray[0];
 
-function pictureLoop() {
-    if (i < picArray.length) {
-        realPic.src = picArray[i]
-        i++
-    } else {
-        i = 0
-    }
+let pictureLoop = function () {
+    if (slidesMoving === true) {
+        
+        if (i < picArray.length) {
+            realPic.src = picArray[i]
+            i++
+        } else {
+            i = 0
+        }
+    } 
 }
 
 let autoStart = setInterval(pictureLoop, 1000);
+let showHolder = document.querySelector('#showHolder');
 
-let showHolder = document.querySelector('#showHolder')
+
 showHolder.onclick = event => {
+
     let target = event.target
+
     console.log(target)
 
-    if(target.classList.contains('start')) {
+    if (target.classList.contains('start')) {
         console.log('start!')
         setInterval(pictureLoop, 1000);
-    } if(target.classList.contains('stop')) {
+        slidesMoving = true
+
+    } else if (target.classList.contains('stop')) {
         console.log('stop')
         clearInterval(autoStart)
+        slidesMoving = false
     }
 }
 
