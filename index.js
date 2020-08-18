5df08f8481f7ee202059faa57f05ffdeaedd8501
Clayton Lewis <lewisclayton89@gmail.com>
2020-08-17 21:47:56 -0400

    practice again

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ console.log('this is seen')
 let navSlide = document.getElementById('slideShow')
 
 navSlide.onclick = () => {
-    window.location.href = '../practice/slideshow.html'
+    window.location.href = '../training-vanilla-js/practice/slideshow.html'
 }
