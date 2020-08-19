0c94aff970f919ee14f15efe34092b252e34d151
Clayton Lewis <lewisclayton89@gmail.com>
2020-08-19 16:44:29 -0400

    dragging works

--- js-files/drag-drop.js
+++ js-files/drag-drop.js
@@ -1,24 +1,47 @@
 let drag = document.getElementById('drag');
 let drop = document.querySelector('.drop-zone')
-let dragging = true;
+let dragging = false;
+console.log(dragging)
+drag.style.display = 'block'
 
 drag.ondragstart = event => {
-    event.dataTransfer.setData('text', event.target.id)
-    console.log('dragging')
-    console.log(event.target.id)
+    event.dataTransfer.setData('text/plain', event.target.id)
 }
 
-drag.ondragover = event => {
+drag.ondrag = event => {
+    let target = event.currentTarget
+    console.log('draggging now')
+    dragging = true
+    if(dragging === true) {
+        target.style.color = 'blue';
+        target.style.display = 'none'
+    } 
+    console.log(dragging)
+}
+
+document.ondragend = event => {
+    dragging = false
+    let target = event.currentTarget
+    if(dragging === false) {
+        drag.style.display = 'block'
+    }
+    console.log('stopped dragging')
+    console.log(dragging)
+}
+
+
+drop.ondragover = event => {
     event.preventDefault()
-    event.dataTransfer.dropEffect = 'move'
     console.log('dragged over')
 }
 
-drop.ondrop = event => {
-   
-        event.preventDefault()
+drop.ondrop = event => {   
+        const id = event.dataTransfer.getData('text');
+        const dragItem = document.getElementById(id);
+        const dropZone = event.target;
+        dropZone.appendChild(dragItem);
+        event.dataTransfer.clearData()
+        dragItem.style.display = 'block'
+        dragItem.style.display = 'red'
         console.log('dropped')
-        let dropData = event.dataTransfer.getData(event.target.id);
-        event.target.appendChild(document.getElementById(dropData))
-
 }
