bac4006aa00d90f0693131b415c45249c4e2811f
Clayton Lewis <lewisclayton89@gmail.com>
2021-01-25 00:53:36 -0500

    image uploader

--- js-files/image-uploader.js
+++ js-files/image-uploader.js
@@ -1,62 +1,53 @@
-const showPic = document.getElementById('show-pic')
-const picZone = document.querySelector('.image-preview');
-const viewImage = document.querySelector('.image-preview-pic');
-const previewMessage = document.querySelector('.preview-message')
+const showPic = document.getElementById("show-pic");
+const picZone = document.querySelector(".image-preview");
+const viewImage = document.querySelector(".image-preview-pic");
+const previewMessage = document.querySelector(".preview-message");
 
-const read = new FileReader()
+//const read = new FileReader();
 
- function showImage (ev) {
-    const file = ev.target.files[0]
-    console.log(file)
+function showImage(ev) {
+  const file = ev.target.files[0];
+  console.log(file);
 
-    if(file) {
+  previewMessage.style.display = "none";
+  viewImage.style.display = "block";
+  viewImage.src = URL.createObjectURL(file)
 
-        previewMessage.style.display = 'none';
-        viewImage.style.display = 'block'
-
-        read.addEventListener('load', (ev) => {
-            viewImage.setAttribute('src', ev.target.result); 
-        })
-
-        read.readAsDataURL(file)
-    } else {
-        previewMessage.style.display = null;
-        viewImage.style.display = null
-    }
 }
 
-showPic.addEventListener('change', showImage)
+showPic.addEventListener("change", showImage);
 
+document.body.ondragover = function (ev) {
+  ev.preventDefault();
+  ev.stopPropagation();
+};
 
+document.body.ondrop = function (ev) {
+  ev.preventDefault();
+  ev.stopPropagation();
+};
+picZone.ondrop = function (ev) {
+  const file = ev.dataTransfer.files[0];
+  console.log(file)
+  
+  previewMessage.style.display = "none";
+  viewImage.style.display = "block";
+  viewImage.src = URL.createObjectURL(file)
+};
 
-document.body.ondragover = (ev) => {
-    ev.preventDefault()
-    ev.stopPropagation()  
-} 
+// Original showImage function code
+/*
+  if (file) {
+    previewMessage.style.display = "none";
+    viewImage.style.display = "block";
 
-document.body.ondrop = (ev) => {
-    ev.preventDefault()
-    ev.stopPropagation()
-} 
-picZone.ondrop = (ev) => {
-    const dt = ev.dataTransfer
-    const file = dt.files[0]
-    console.log(file)
+    read.addEventListener("load", (ev) => {
+      viewImage.setAttribute("src", ev.target.result);
+    });
 
-    if(file) {
-
-        previewMessage.style.display = 'none';
-        viewImage.style.display = 'block'
-
-        read.addEventListener('load', (ev) => {
-            viewImage.setAttribute('src', ev.target.result); 
-        })
-
-        read.readAsDataURL(file)
-    } else {
-        previewMessage.style.display = null;
-        viewImage.style.display = null
-    }
-}
-
-//picZone.addEventListener('drop', showImage)
+    read.readAsDataURL(file);
+  } else {
+    previewMessage.style.display = null;
+    viewImage.style.display = null;
+  }
+  */
